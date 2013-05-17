8a094052455582ba3980028de74acc6bc9a3206c
João Daniel <jdanielnd@gmail.com>
2013-05-17 11:42:44 -0300

    fixing gzip command

--- lib/pod_rails.rb
+++ lib/pod_rails.rb
@@ -18,7 +18,7 @@ module PodRails
     server ||= "http://pod.fores.ee"
     # uri = URI.parse("#{PodRails.config.server}/convert/")
     uri = URI.parse("#{server}/convert/")
-    compressed_html = ActiveSupport::Gzip::Stream.compress(options[:document_content])
+    compressed_html = ActiveSupport::Gzip.compress(options[:document_content])
     # params = {filename: options[:name], html: options[:document_content]}
     params = {filename: options[:name], html: compressed_html}
     begin
